a3642afaff2b4d5b9d70aaf7e16902297ab88722
Daniel Akoto <sult4nbiz@gmail.com>
2021-11-12 00:30:51 -0500

    almost done app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MyPortfolio
 this is going to be my portfolio using my landing page template(you can find that on my GitHub) 
+
+Link: https://myportfolio-bb961.web.app
--- frontend/static/js/views/Profile.js
+++ frontend/static/js/views/Profile.js
@@ -14,14 +14,13 @@ export default class extends AbstractView {
             <div class="whitebox2">
                 <img style="border-radius: 5%; width: 25%; margin-top: 30px;" src="/static/pictures/profilepic.jpg" alt="">
                 <h1 style="font-size: 28px;">Daniel Akoto</h1>
-                <h3 style=" margin-left: 4%; ">Towson University</h3>
-                <h3 style=" margin-left: 4%; font-size: 22px; margin-bottom: 5px;"> Student</h3>
+                <h3>Towson University</h3>
+                <h3 style="font-size: 22px; margin-bottom: 5px;">Student</h3>
                 <p> I love solving puzzles and problem-solving challenges. Making programs has opened my mind to endless possibilities, I look forward to creating applications that can improve our daily lives and help us live better and happier through technology.</p>
                 <h4 style="font-family: 'Source Sans Pro', sans-serif; float: left; font-style: italic; margin-left: 4%;">Maryland, US</h4>
-                <h4>sult4nbiz@gmail.com</h4>
+                <h4>dakoto1@students.towson.edu</h4>
                 <div style="margin-bottom: 30px;"></div>
             </div>
-            
         </center>
         </div>
         </a>
