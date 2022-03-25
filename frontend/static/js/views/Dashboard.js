f35b0c72f38674eff90b8c08628c06d45273f932
Daniel Akoto <sult4nbiz@gmail.com>
2022-03-24 22:13:04 -0400

    updating profile tab display to show contents in the center of the screen

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor() {
         super();
-        this.setTitle("Dashboard")
+        this.setTitle("D.A: Dashboard")
     }
 
     async getHtml() {
@@ -54,38 +54,42 @@ export default class extends AbstractView {
         <!-- this is the layout for all your projects, edit to fit your needs -->
         <div class="whitebox">
             <a target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;" href="https://chrome.google.com/webstore/detail/phoebe-notes/hoikdhcpdlhbhfgmcpdhdhbkjhenncje">
-            <img src="/static/pictures/phoebe-notes-small-banner-440-280.png" alt="" style="width: 100%;">
-            <h1>Phoebe Notes Extension</h1>
-            <p>Phoebe is a note taking chrome extension that allows users to simply take notes.</p>
+                <img src="/static/pictures/phoebe-notes-small-banner-440-280.png" alt="" style="width: 100%;">
+                <h1>Phoebe Notes Extension</h1>
+                <p>A note taking chrome extension that allows users to simply take notes.</p>
             </a>
-        </div> 
+        </div>
+        <br>
         <div class="whitebox">
             <a target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;" href=" https://yt-pic-in-pic.web.app">
-            <img src="/static/pictures/ytpic.jpg" alt="" style="width: 100%;">
-            <h1>Pic-in-Pic</h1>
-            <p>Picture in picture on mobile devices without YouTube premium subscription. This is a mobile app and only works for chrome based apps(chrome, edge, firefox, etc) THIS APP DOES NOT WORK ON SAFARI</p>
-        </a>
+                <img src="/static/pictures/ytpic.jpg" alt="" style="width: 100%;">
+                <h1>Pic-in-Pic</h1>
+                <p>Picture in picture on mobile devices without YouTube premium subscription.</p>
+            </a>
         </div>
+        <br>
         <div class="whitebox">
             <a target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;" href="https://havingfun-c94bc.web.app">
-            <img src="/static/pictures/havingfunpic.jpg" alt="" style="width: 100%;">
-            <h1>Having Fun</h1>
-            <p>A game about remembering the numbers The game gets harder as you memorize faster and faster</p>
-        </a>
+                <img src="/static/pictures/havingfunpic.jpg" alt="" style="width: 100%;">
+                <h1>Having Fun</h1>
+                <p>A game about remembering the numbers.</p>
+            </a>
         </div>
+        <br>
         <div class="whitebox">
             <a target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;" href="https://catchdodgegame.web.app/">
-            <img src="/static/pictures/catchpic.jpg" alt="" style="width: 100%;">
-            <h1>Catch Dodge Game</h1>
-            <p>Catch the black balls and dodge the white balls. THIS GAME IS NOT MOBILE FRIENDLY</p>
-        </a>
+                <img src="/static/pictures/catchpic.jpg" alt="" style="width: 100%;">
+                <h1>Catch Dodge Game</h1>
+                <p>Catch the black balls and dodge the white balls. Computer Only</p>
+            </a>
         </div>
+        <br>
         <div class="whitebox">
             <a target="_blank" rel="noopener noreferrer" style="text-decoration: none; color: black;" href="https://landingpagetemplate-edf22.web.app">
-            <img src="/static/pictures/landingpic.jpg" alt="" style="width: 100%;">
-            <h1>Landing Page Template</h1>
-            <p>This is a template to help build a stunning landing page. Note : This is not a library.</p>
-        </a>
+                <img src="/static/pictures/landingpic.jpg" alt="" style="width: 100%;">
+                <h1>Landing Page Template</h1>
+                <p>A template to help build a stunning landing page. Note: This is not a library.</p>
+            </a>
         </div>
         </center>
         <br>
--- frontend/static/js/views/Profile.js
+++ frontend/static/js/views/Profile.js
@@ -3,12 +3,11 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor() {
         super();
-        this.setTitle("Profile")
+        this.setTitle("D.A: Profile")
     }
 
     async getHtml() {
         return `
-        <a href="https://www.linkedin.com/in/daniel-akoto-101" target="_blank">
         <div class="profile">
         <center>
             <div class="whitebox2">
@@ -16,14 +15,16 @@ export default class extends AbstractView {
                 <h1 style="font-size: 28px;">Daniel Akoto</h1>
                 <h3>Towson University</h3>
                 <h3 style="font-size: 22px; margin-bottom: 5px;">Student</h3>
-                <p> I love solving puzzles and problem-solving challenges. Making programs has opened my mind to endless possibilities, I look forward to creating applications that can improve our daily lives and help us live better and happier through technology.</p>
-                <h4 style="font-family: 'Source Sans Pro', sans-serif; float: left; font-style: italic; margin-left: 4%;">Maryland, US</h4>
-                <h4>dakoto1@students.towson.edu</h4>
-                <div style="margin-bottom: 30px;"></div>
+                <p> I love solving puzzles and problem-solving challenges. I look forward to creating applications that can improve our daily lives and help us live better and happier through technology.</p>
+            
+                <div class="profile-bottom">
+                    <a id="mailto" href="mailto:da.akoto1@gmail.com">da.akoto1@gmail.com</a>
+                    <a id="resume" href="../../files/Daniel-Akoto-Resume.10.pdf" target="_blank">Resume</a>
+                </div>
             </div>
+            <br>
         </center>
         </div>
-        </a>
         `;
     }
 }
